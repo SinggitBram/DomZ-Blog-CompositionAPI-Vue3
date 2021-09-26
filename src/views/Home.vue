6a85f6b861d72c29d88b630346eb3b8641cc6fd5
SinggitBram <bambadom@gmail.com>
2021-09-27 04:51:01 +0700

    add post to blog with composition API

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,37 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div v-if="error">{{ error }}</div>
+    <div v-if="posts.length">
+      <PostList :posts="posts" />
+    </div>
+    <div v-else>
+      <Spinner />
+    </div>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
+import { ref } from 'vue'
+import getPosts from '../composables/getPosts'
+// component imports
+import PostList from '../components/PostList.vue'
+import Spinner from '../components/Spinner.vue'
 export default {
   name: 'Home',
-  components: {
-    HelloWorld
-  }
+  components: { PostList, Spinner },
+  setup() { 
+    const { posts, error, load } = getPosts()
+    load()
+    
+    return { posts, error }
+  },
 }
 </script>
+
+<style>
+  .home {
+    max-width: 1200px;
+    margin: 0 auto;
+    padding: 10px;
+  }
+</style>
